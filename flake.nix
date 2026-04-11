fb590f03ef10eda881e8466c2309a000e9b67e5e
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-04-11 12:37:37 +0300

    chore: use Nix for CI builds and add packages.default to flake
    Replace actions/setup-go and golangci-lint-action with Nix installer
    and cache actions, run all commands via nix develop. Add buildGoModule
    package output and build-release job for multi-platform binary uploads.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "sqlc-gen-queries development environment";
+  description = "sqlc-gen-queries - SQLC Queries Generator";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -11,14 +11,29 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgs.legacyPackages.${system};
+        version = (pkgs.lib.importJSON ./.github/config/release-please-manifest.json).".";
       in
       {
+        packages.default = pkgs.buildGoModule {
+          pname = "sqlc-gen-queries";
+          inherit version;
+          src = pkgs.lib.cleanSource ./.;
+          subPackages = [ "cmd/sqlc-gen-queries" ];
+          vendorHash = pkgs.lib.fakeHash; # replace with real hash after running `nix build`
+          meta = with pkgs.lib; {
+            description = "SQLC Queries Generator";
+            license = licenses.mit;
+            mainProgram = "sqlc-gen-queries";
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           name = "sqlc-gen-queries";
-          packages = [
-            pkgs.go_1_25
-            pkgs.gopls
+          packages = with pkgs; [
+            go
+            golangci-lint
+            gopls
           ];
         };
       }
